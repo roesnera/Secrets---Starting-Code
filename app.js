5bc2646503d36c4503bea776b64ace5c419717ae
Adam Roesner <roesnera@kenyon.edu>
2022-03-06 18:37:40 -0500

    Adds login with google option

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const encrypt = require('mongoose-encryption');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocalMongoose = require('passport-local-mongoose');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
 const app = express();
 app.use(express.static('public'));
@@ -34,10 +36,14 @@ const userSchema = mongoose.Schema({
     },
     password: {
         type: String
+    },
+    googleId: {
+        type: String
     }
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 // USERDBKEY set in .env file and compiled into environment variables using dotenv package
 // const secret = process.env.USERDBKEY;
@@ -49,11 +55,40 @@ const User = mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    })
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 app.get('/', function(req, res) {
     res.render('home');
+});
+
+app.get('/auth/google', 
+    passport.authenticate('google', { scope: ['profile'] })
+);
+
+app.get('/auth/google/secrets', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
 });
 
 app.get('/login', function(req, res){
