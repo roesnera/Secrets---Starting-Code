abcdbc404c9fc12e762b682a99a794d7364c175a
Adam Roesner <roesnera@kenyon.edu>
2022-03-06 14:33:10 -0500

    Basic pathway from home route to secrets, login and register work but not encrypted.

---
+++ app.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+
+const app = express();
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+const yourDB = 'userDB';
+const url = `mongodb://localhost:27017/${yourDB}`;
+mongoose.connect(url);
+
+const userSchema = mongoose.Schema({
+    email: {
+        type: String
+    },
+    password: {
+        type: String
+    }
+});
+
+const User = mongoose.model("User", userSchema);
+
+app.get('/', function(req, res) {
+    res.render('home');
+});
+
+app.get('/login', function(req, res){
+    res.render('login');
+});
+
+app.post('/login', function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username}, function(err, foundName){
+        if(err){
+            console.log(err);
+            res.render('login');
+        } else {
+            if(foundName){
+                if(foundName.password === password){
+                    res.render('secrets');
+                }
+            }
+        }
+    });
+    res.render('login');
+})
+
+app.get('/register', function(req, res){
+    res.render('register');
+});
+
+app.post('/register', function(req, res){
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    newUser.save(function(err){
+        if(err){
+            console.log(err);
+        } else {
+            res.render('secrets');
+        }
+    })
+});
+
+app.listen(3000, function() {
+    console.log('Server lsitening on port 3000')
+})
